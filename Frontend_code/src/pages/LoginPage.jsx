beba2adb1621d122f12b08dd52575d76e83b8db9
sasgroups <saiganesh@sasgroups.net>
2026-04-17 11:44:12 +0530

    login_update

--- Frontend_code/src/pages/LoginPage.jsx
+++ Frontend_code/src/pages/LoginPage.jsx
@@ -107,7 +107,7 @@ export default function LoginPage() {
         {/* <AutoKioskSender /> */}
 
         <h1 className="text-3xl font-bold text-center text-slate-800 mb-6">
-          LOGIN
+          Login
         </h1>
 
         {/* --- Tabs --- */}
